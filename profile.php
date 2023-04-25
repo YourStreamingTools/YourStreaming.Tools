fc36ba74f2e0a3c24375fbd51da46d4516ce4b36
gfaUnDead <contact@gfaundead.stream>
2023-04-25 14:40:27 +1000

    Update profile.php

--- profile.php
+++ profile.php
@@ -67,7 +67,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <nav class="navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="../index.php">YourListOnline</a>
+            <a class="navbar-brand" href="../index.php">YourStreamingTools</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="dashboard.php">Dashboard</a></li>
