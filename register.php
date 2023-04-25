1ee8f94d46bc5e5fca2d64e661f8cff307fd4d1b
gfaUnDead <contact@gfaundead.stream>
2023-04-25 14:39:55 +1000

    Update register.php

--- register.php
+++ register.php
@@ -119,7 +119,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && $registration_enabled){
     <nav class="navbar navbar-default">
         <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="../index.php">YourListOnline</a>
+                <a class="navbar-brand" href="../index.php">YourStreamingTools</a>
             </div>
             <ul class="nav navbar-nav">
                 <li><a href="index.php">Home</a></li>
