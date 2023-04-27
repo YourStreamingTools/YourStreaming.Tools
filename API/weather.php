a3665c98c489d5fc867e715f504cef3f6f4279de
GFAUnDead <contact@gfaundead.stream>
2023-04-27 12:51:40 +1000

    Update weather.php

--- API/weather.php
+++ API/weather.php
@@ -1,4 +1,48 @@
 <?php
+// Check if the API key is specified in the URL
+if (isset($_GET['api'])) {
+    // Retrieve the API key from the URL
+    $api_key = $_GET['api'];
+} else {
+    // Return an error message if the API key is not specified in the URL
+    echo "API key is required.";
+    exit();
+}
+
+// Require database connection
+require_once "db_connect.php";
+
+// Check if the connection is successful
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare the SQL statement to retrieve the channel name and username for the given API key
+$stmt = $conn->prepare("SELECT channelname FROM allowed_users WHERE api_key = ?");
+$stmt->bind_param("s", $api_key);
+
+// Execute the SQL statement
+$stmt->execute();
+
+// Bind the result to variables
+$stmt->bind_result($channelname);
+
+// Fetch the result
+$stmt->fetch();
+
+// Close the statement
+$stmt->close();
+
+// Close the database connection
+$conn->close();
+
+// Check if the provided API key is valid and retrieve the channel name from the database
+if (empty($channelname)) {
+    // Return an error message if the API key is not valid
+    echo "Invalid API key.";
+    exit();
+}
+
 // Check if the units and city are specified in the URL
 $units = $_GET['units'] ?? 'metric';
 $city = $_GET['city'] ?? 'Sydney';
