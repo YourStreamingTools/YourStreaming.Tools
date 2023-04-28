61702678d98e2d25c3d46ab9abfd7062c4ad42c1
gfaUnDead <contact@gfaundead.stream>
2023-04-29 02:38:33 +1000

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -44,7 +44,7 @@ if (isset($_POST['update_username'])) {
     $stmt->execute();
 
     // Redirect to profile page
-    header("Location: profile.php");
+    header("Location: logout.php");
     exit();
 }
 
@@ -120,10 +120,10 @@ $conn->close();
     <form id="update-profile-image-form" action="update_profile.php" method="POST">
         <h2>Update Profile Image</h2>
         <div>
-        <img id="profile-image" src="<?php echo $_SESSION['twitch_profile_image_url']; ?>" width="100px" height="100px" alt="New Profile Image">
+        <img id="profile-image" src="<?php echo $twitch_profile_image_url; ?>" width="100px" height="100px" alt="New Profile Image">
         </div>
         <div>
-            <input type="hidden" name="twitch_profile_image_url" value="<?php echo $_SESSION['twitch_profile_image_url']; ?>">
+            <input type="hidden" name="twitch_profile_image_url" value="<?php echo $twitch_profile_image_url; ?>">
             <button class="btn btn-primary" id="update-profile-image-button" name="update_profile_image">Update New Profile Image</button>
         </div>
     </form>
