af14c173051dd66b09a4547c04223771d7eba7ea
gfaUnDead <contact@gfaundead.stream>
2023-04-28 23:56:05 +1000

    Update register.php

--- register.php
+++ register.php
@@ -93,8 +93,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && $registration_enabled){
 <head>
     <meta charset="UTF-8">
     <title>YourStreamingTools - Sign Up</title>
-    <link rel="icon" href="img/logo.png" type="image/png" />
-    <link rel="apple-touch-icon" href="img/logo.png">
+    <link rel="icon" href="img/logo.ico" type="image/png" />
+    <link rel="apple-touch-icon" href="img/logo.ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
