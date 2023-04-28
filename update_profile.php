305bd0307b7c56e893a251ecab613014864c6caa
gfaUnDead <contact@gfaundead.stream>
2023-04-28 23:56:06 +1000

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -63,8 +63,8 @@ $conn->close();
 <html>
 <head>
   <title>YourStreamingTools - Update Profile</title>
-  <link rel="icon" href="img/logo.png" type="image/png" />
-  <link rel="apple-touch-icon" href="img/logo.png">
+  <link rel="icon" href="img/logo.ico" type="image/png" />
+  <link rel="apple-touch-icon" href="img/logo.ico">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
